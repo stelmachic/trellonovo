60e27654531c8e453498247d95a71abfc7ed25fd
Nicholas <nicholasstelmach@icloud.com>
2019-04-04 17:22:21 -0300

    Depositando banco de dados

---
+++ sistema_tar.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04-Abr-2019 às 22:21
+-- Versão do servidor: 10.1.36-MariaDB
+-- versão do PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistema_tar`
+--
+CREATE DATABASE IF NOT EXISTS `sistema_tar` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sistema_tar`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tarefas`
+--
+
+CREATE TABLE `tarefas` (
+  `id_tarefa` int(2) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `data` date NOT NULL,
+  `tipo` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tarefas`
+--
+
+INSERT INTO `tarefas` (`id_tarefa`, `descricao`, `data`, `tipo`) VALUES
+(1, 'OIIOIOIO', '2019-04-09', 1),
+(2, 'teste', '2019-04-02', 1),
+(3, 'Fazer trabalho', '2019-04-19', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD PRIMARY KEY (`id_tarefa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tarefas`
+--
+ALTER TABLE `tarefas`
+  MODIFY `id_tarefa` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
